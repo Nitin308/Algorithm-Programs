b520c2f51583edcb53cf4948243651f52952cadf
Nitin308 <nitin3081998@gmail.com>
2022-12-07 14:33:00 +0530

    Prime Numbers within a range

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+            PrimeNumbers obj = new PrimeNumbers();
+            obj.PrimeNumber();
+            Console.ReadLine();
+        }
+    }
+}
