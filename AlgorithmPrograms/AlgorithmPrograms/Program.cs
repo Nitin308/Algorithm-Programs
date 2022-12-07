02cae0342e26b258e57e5d010e4b15accb6c4e30
Nitin308 <nitin3081998@gmail.com>
2022-12-07 11:52:49 +0530

    Binary Search the word from word list

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchWord obj = new BinarySearchWord();
+            obj.WordExistsOrNot();
+        }
+    }
+}
