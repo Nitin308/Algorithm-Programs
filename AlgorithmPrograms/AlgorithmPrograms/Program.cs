e8243b930b5b7a2cb5f63f5bed2004a203ed0b20
Nitin308 <nitin3081998@gmail.com>
2022-12-07 14:59:02 +0530

    Find Prime Numbers that are Anagram and Palindrome in a given range

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+            PrimeNumbersThatAreAnagramAndPalindrome obj = new PrimeNumbersThatAreAnagramAndPalindrome();
+            obj.PrimeNumber();
+            Console.ReadLine();
+        }
+    }
+}
