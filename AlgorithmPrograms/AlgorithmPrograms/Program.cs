c9a944440ca5e89d50c0da532039d66df47b5646
Nitin308 <nitin3081998@gmail.com>
2022-12-07 12:59:46 +0530

    Bubble Sort

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+
+            int[] array = { 51, 12, 49, 26, 85, 153, 4 };
+            BubbleSortMethod objSort = new BubbleSortMethod();
+
+            Console.WriteLine("\nBefore sorting array elements are: ");
+            objSort.Display(array);
+
+            objSort.Sort(array);
+            Console.WriteLine("\n\nAfter sorting array elements are: ");
+            objSort.Display(array);
+
+            Console.ReadLine();
+        }
+    }
+}
