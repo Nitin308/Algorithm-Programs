329145ae60592d354e0863aec3e6b1f8a1484cbb
Nitin308 <nitin3081998@gmail.com>
2022-12-07 14:00:12 +0530

    Anagram Detection Program

---
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+            AnagramProgram obj = new AnagramProgram();
+            obj.AnagramDetectionProgram();
+            Console.ReadLine();
+        }
+    }
+}
